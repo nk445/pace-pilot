6f98830a09aa5dc4a86a88aba65ddee48fd7e365
Nahom Kibreab <143555609+nk445@users.noreply.github.com>
2026-09-23 17:01:05 -0400

    Finish declaring type aliases for relevant user inputs and data models

--- lib/types.ts
+++ lib/types.ts
@@ -3,7 +3,36 @@ type UserInput = {
   targetMileage?: number;
   weeks: number;
   raceDistance?: number;
-  raceUnit: 'mi' | 'km' | 'm';
+  raceUnit: 'mi' | 'km';
   raceTimeInSeconds?: number;
   sessions: number;
 };
+
+type Run = {
+    runId: string;
+    distance: number;
+    unit: 'mi' | 'km';
+    type: 'easy' | 'threshold' | 'interval' | 'repetition' | 'long';
+}
+
+// Day: Day of the week name/index, list of Run objects, total daily distance.
+type Day = {
+    dayOfWeek: 'Monday' | 'Tuesday' | 'Wednesday' | 'Thursday' | 'Friday' | 'Saturday' | 'Sunday';
+    runList: Run[];
+    totalDistance: number;
+}
+
+// Week: Week number, total target mileage, list of Day objects.
+type Week = {
+    weekNumber: number;
+    weeklyMileage: number;
+    days: Day[];
+}
+
+// Schedule: Unique ID, creation date, user inputs used, list of Week objects.
+type Schedule = {
+    scheduleId: string;
+    creationDate: Date;
+    inputs: UserInput;
+    weeks: Week[];
+}

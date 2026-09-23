b56582f81d32d8046c2c3f6e812c897776aae1be
Nahom Kibreab <143555609+nk445@users.noreply.github.com>
2026-09-23 16:47:24 -0400

    Declare initial data models as Typescript types

---
+++ lib/types.ts
@@ -0,0 +1,9 @@
+type UserInput = {
+  baseMileage?: number;
+  targetMileage?: number;
+  weeks: number;
+  raceDistance?: number;
+  raceUnit: 'mi' | 'km' | 'm';
+  raceTimeInSeconds?: number;
+  sessions: number;
+};
